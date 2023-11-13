f003e12eac1e0f03599c0565d9780e849a461d60
Michael Reimann <michael@reimann.science>
2023-11-13 07:09:04 -0500

    Added analysis of SSCX model. Also: Summary statistics

--- helpers/network.py
+++ helpers/network.py
@@ -1,6 +1,7 @@
 import conntility
 import pandas
 import numpy
+import bluepysnap as snap
 import json
 
 def load_network(cfg):
@@ -8,7 +9,24 @@ def load_network(cfg):
         fn = cfg["conntility"]
         return conntility.ConnectivityMatrix.from_h5(fn, *cfg.get("args", []), **cfg.get("kwargs", {}))
     elif "snap" in cfg.keys():
-        raise NotImplementedError()
+        load_cfg = {
+            "loading":{    
+                "properties": ["x", "y", "z", "mtype", "layer", "synapse_class"],
+                "atlas": [
+                    {
+                        "data": cfg["snap"]["flatmap"],
+                        "properties": ["ss_flat_x", "depth", "ss_flat_y"]
+                    }
+                ]
+            }
+        }
+        connectome = cfg["snap"]["connectome"]
+        fn_circ = cfg["snap"]["circuit"]
+        circ = snap.Circuit(fn_circ)
+        cmat = conntility.ConnectivityMatrix.from_bluepy(circ, load_config=load_cfg,
+                                                        connectome=connectome,
+                                                        load_full=True)
+        return cmat
 
 def filter_network(M, cfg):
     for fltr in cfg:
